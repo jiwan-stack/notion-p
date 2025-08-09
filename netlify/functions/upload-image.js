e49159bf474fccbf73b4722cc20e30758249d670
Jiwan ghosal <jiwan.cse@gmail.com>
2025-08-09 13:03:37 +0530

    feat: upload iamge

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -1,25 +1,31 @@
 import Busboy from "busboy";
+import fs from "fs";
+import path from "path";
 
-export async function handler(event) {
-  return new Promise((resolve, reject) => {
-    const busboy = Busboy({ headers: event.headers });
-    const files = [];
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send({ error: "Method not allowed" });
+  }
 
-    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-      const chunks = [];
-      file.on("data", (data) => chunks.push(data));
-      file.on("end", () => {
-        files.push({ filename, content: Buffer.concat(chunks), mimetype });
-      });
-    });
+  const uploadsDir = path.join(process.cwd(), "uploads");
+  if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir);
+  }
 
-    busboy.on("finish", () => {
-      resolve({
-        statusCode: 200,
-        body: JSON.stringify({ files }),
-      });
-    });
+  const files = [];
 
-    busboy.end(Buffer.from(event.body, "base64"));
+  const busboy = Busboy({ headers: req.headers });
+
+  busboy.on("file", (fieldname, file, filename) => {
+    const savePath = path.join(uploadsDir, filename);
+    const writeStream = fs.createWriteStream(savePath);
+    file.pipe(writeStream);
+    files.push(filename);
   });
+
+  busboy.on("finish", () => {
+    res.status(200).json({ message: "Files uploaded", files });
+  });
+
+  req.pipe(busboy);
 }
